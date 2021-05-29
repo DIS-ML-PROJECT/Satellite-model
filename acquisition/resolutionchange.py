f2b5992d64ad28a40f84a3289e51ba2f178ebbba
EstrixDS <49279931+EstrixDS@users.noreply.github.com>
2021-05-29 03:20:37 +0200

    updated the script (memoryerror at file 4092)

--- acquisition/resolutionchange.py
+++ acquisition/resolutionchange.py
@@ -2,12 +2,14 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
-dirpath = 'C:/Users/esser/sciebo/Data_Acquisition/nightlight'
+dirpath = 'D:/satellite/data/nightlight'
+exportpath = 'D:/satellite/data/nightlightrightres/'
 
 onlyfiles = [f for f in listdir(dirpath) if isfile(join(dirpath, f))]
 for i in onlyfiles:
     imgfile = dirpath + "/" + i
-    image = Image.open(imgfile)
-    image = image.resize((1100, 1100))
-    image.save(imgfile)
+    with open(imgfile,'rb') as f:
+        image = Image.open(f)
+        image = image.resize((1100, 1100))
+        image.save(exportpath + i)
     print(onlyfiles.index(i))
