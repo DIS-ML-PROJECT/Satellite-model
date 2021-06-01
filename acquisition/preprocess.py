bd010c5f836fcfd425c5313ff0353451f603eb2b
EstrixDS <49279931+EstrixDS@users.noreply.github.com>
2021-06-01 13:00:34 +0200

    Update preprocess.py
    deleted unnecessesary Libraries, added additional features and definitions, cropped the image to 1050x1050 to not augment data

--- acquisition/preprocess.py
+++ acquisition/preprocess.py
@@ -2,7 +2,6 @@ from PIL import Image
 import os
 from os import listdir
 from os.path import isfile, join
-import copy
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -27,15 +26,41 @@ def _float_feature(value):
   """Returns a float_list from a float / double."""
   return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
-def serialize_example(nparray,label):
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def serialize_example(nparray,wealth,wealthpooled,wealthpooled5country,country,urbanrural,lon_coord,lat_coord,year):
   """
   Creates a tf.train.Example message ready to be written to a file.
   """
   # Create a dictionary mapping the feature name to the tf.train.Example-compatible
   # data type.
   feature = {
+      #image (vorhanden)
       'nparray': _dtype_feature(nparray),
-      'label': _float_feature(label),
+      #wealthindex (vorhanden)
+      'wealth': _float_feature(wealth),
+      #wealthpooled
+      'wealthpooled': _float_feature(wealthpooled),
+      #wealthpooled5country
+      'wealthpooled5country': _float_feature(wealthpooled5country),
+      #country of survey (vorhanden)
+      'country': _bytes_feature(country),
+      # "U" for Urban, "R" for rural (vorhanden)
+      'urbanrural': _bytes_feature(urbanrural),
+      # Longitude Coords of the Center of the image (vorhanden)
+      'centerlon': _float_feature(lon_coord),
+      # Latitude Coords of the Center of the image (vorhanden)
+      'centerlat': _float_feature(lat_coord),
+      # year of the survey (vorhanden)
+      'year': _int64_feature(year)
   }
   # Create a Features message using tf.train.Example.
   example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
@@ -46,6 +71,7 @@ dirpath = 'D:/satellite/data/nightlight'
 exportpath = 'D:/satellite/data/tfrecords/'
 
 onlyfiles = [f for f in listdir(dirpath) if isfile(join(dirpath, f))]
+onlyfiles = onlyfiles[:10]
 csvpath = os.path.abspath(os.path.join(os.path.abspath(__file__),"../../dataResearch/firstSample.csv"))
 df = pd.read_csv(csvpath)
 for i in onlyfiles:
@@ -53,15 +79,15 @@ for i in onlyfiles:
     with open(imgfile,'rb') as f:
         image = Image.open(f)
         image.load()
-        image = image.resize((1100, 1100))
+        image = image.resize((1050, 1050))
         imagearray = np.array(image.getdata())
         #Normalizing
         normalizedimagearray = stats.zscore(imagearray)
-        imagearray = normalizedimagearray
+        #Get additional Features from CSV-File
         labelname = i.replace(".tif","")
-        n = float(df["wealth"].at[df.ID[df.ID == labelname].index[0]].replace(",","."))
+        wealth = float(df["wealth"].at[df.ID[df.ID == labelname].index[0]].replace(",","."))
         with tf.io.TFRecordWriter(exportpath + labelname + '.tfrec') as writer:
-            example = serialize_example(nparray=imagearray,label=n)
+            example = serialize_example(nparray=normalizedimagearray,wealth=wealth)
             writer.write(example)
         f.close()
         image.close()
